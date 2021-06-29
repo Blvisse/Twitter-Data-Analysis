5066407a99ad997fc443677f389572cf07369fb3
Blvisse <blaisepke@gmail.com>
2021-06-29 12:22:34 +0300

    modified modules and functions

--- extract_dataframe.py
+++ extract_dataframe.py
@@ -233,7 +233,7 @@ if __name__ == "__main__":
     # required column to be generated you should be creative and add more features
     columns = ['created_at', 'source', 'original_text','clean_text', 'sentiment','polarity','subjectivity', 'lang', 'favorite_count', 'retweet_count', 
     'original_author', 'screen_count', 'followers_count','friends_count', 'hashtags', 'user_mentions', 'place', 'place_coord_boundaries']
-    _, tweet_list = read_json(r"C:\Users\blais\Desktop\10-Academy\Task1\Twitter-Data-Analysis\data\covid19.json")
+    _, tweet_list = read_json(r"data\covid19.json")
     tweet = TweetDfExtractor(tweet_list)
     tweet_df = tweet.get_tweet_df() 
 
